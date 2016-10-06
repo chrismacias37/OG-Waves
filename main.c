02eb09b116e1f1a1464a673ae58c6d0edd4627e6
Christian Macias <chrismacias37@gmail.com>
2016-10-06 11:17:49 -0600

    Added basic i2c functions

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 /*
  * main.c
  */
+#include<driverlib/sysctl.h>
 #include"hardware/gpio.h"
-#include"hardware/i2c.h"
 
 int main(void) {
 
-	initalize_pins();
-	initalize_i2c();
+	ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);//Set the proccessor speed to 5Mhz
+	I2CMasterInitExpClk(I2C0_BASE,SysCtlClockGet(),1); //Set up I2C0 Module to master mode and set it to 400kbps
+
 
 }
---
+++ sensors/accelerometer.c
@@ -0,0 +1,9 @@
+/*
+ * Description:
+ * Functions to control the LSM303DLHC's accelerometer
+ *
+ *  Created on: Oct 5, 2016
+ *      Author: Christian Macias
+ */
+
+#include"accelerometer.h"
