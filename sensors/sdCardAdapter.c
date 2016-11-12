b4f2a9cac5ef717182097e14b79008f3e1632061
Christian Macias <chrismacias37@gmail.com>
2016-11-12 11:45:08 -0700

    new file:   sensors/SD_Card/diskio.c 	new file:   sensors/SD_Card/diskio.h 	new file:   sensors/SD_Card/ff.c 	new file:   sensors/SD_Card/ff.h 	new file:   sensors/SD_Card/ffconf.h 	new file:   sensors/SD_Card/integer.h 	modified:   sensors/sdCardAdapter.c 	modified:   .cproject 	modified:   .settings/org.eclipse.core.resources.prefs

--- sensors/sdCardAdapter.c
+++ sensors/sdCardAdapter.c
@@ -7,9 +7,10 @@
 #include <inc/hw_memmap.h>
 #include <driverlib/sysctl.h>
 #include"sdCardAdapter.h"
+#include"SD_Card/sd"
 
 int mmc_init(int base_address)
 {
-	SSIConfigSetExpClk(SSI0_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_0, SSI_MODE_MASTER, 200, 11);
+	SSIConfigSetExpClk(SSI0_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_0, SSI_MODE_MASTER, 200000, 64);//Use SSI in Motorola mode 0 (AKA SPI), set clock rate to 200kHz, and set 64bit frame.
 	SSIEnable(SSI0_BASE);
 }
